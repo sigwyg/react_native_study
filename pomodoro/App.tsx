c3f39f83f4d886632420f64513277a80709ed8d9
Yasuo Fukuda <yasuo.fukuda@theport.jp>
2020-06-25 17:33:19 +0900

    fix type

--- pomodoro/App.tsx
+++ pomodoro/App.tsx
@@ -1,38 +1,46 @@
-import React from 'react';
-import { AppLoading } from 'expo';
+import React from 'react'
+import { AppLoading } from 'expo'
 import {
-    Container,
-    Header,
-    Content,
-    Footer,
-    FooterTab,
-    Button,
-    Body,
-    Left,
-    Right,
-    Text,
-    Grid,
-    Row,
-    Col,
-    Form,
-    Item,
-    Input,
-    Label,
-    Card,
-    CardItem
-} from 'native-base';
-import * as Font from 'expo-font';
-import { Ionicons } from '@expo/vector-icons';
-import Apples from './components/Apples.tsx'
+  Container,
+  Header,
+  Content,
+  Footer,
+  FooterTab,
+  Button,
+  Body,
+  Left,
+  Right,
+  Text,
+  Grid,
+  Row,
+  Col,
+  Form,
+  Item,
+  Input,
+  Label,
+  Card,
+  CardItem,
+} from 'native-base'
+import * as Font from 'expo-font'
+import { Ionicons } from '@expo/vector-icons'
+import Apples from './components/Apples'
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
+interface Props {}
+
+interface State {
+  isReady: boolean
+  title: string
+  tasks: Array<string>
+}
+
+export default class App extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
     this.state = {
       isReady: false,
       title: '',
-      tasks: []
-    };
+      tasks: [],
+    }
     this.handleChange = this.handleChange.bind(this)
     this.addTask = this.addTask.bind(this)
   }
@@ -42,88 +50,96 @@ export default class App extends React.Component {
       Roboto: require('native-base/Fonts/Roboto.ttf'),
       Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
       ...Ionicons.font,
-    });
-    this.setState({ isReady: true });
+    })
+    this.setState({ isReady: true })
   }
 
-  handleChange(e) {
+  handleChange(
+    e: NativeSyntheticEvent<TextInputChangeEventData> | React.ChangeEvent
+  ) {
     const title = e.target.value || e.nativeEvent.text
 
-    this.setState({ title: title });
+    this.setState({ title: title })
   }
 
   addTask() {
     const newTask = this.state.title
-    this.setState({ tasks: [...this.state.tasks, newTask]})
+    this.setState({ tasks: [...this.state.tasks, newTask] })
   }
 
   render() {
     if (!this.state.isReady) {
-      return <AppLoading />;
+      return <AppLoading />
     }
 
     return (
       <Container>
         <Header>
-            <Left>
-                <Text>🍎</Text>
-            </Left>
-            <Body>
-                <Text>Header</Text>
-            </Body>
-            <Right>
-                <Text>🍏</Text>
-            </Right>
+          <Left>
+            <Text>🍎</Text>
+          </Left>
+          <Body>
+            <Text>Header</Text>
+          </Body>
+          <Right>
+            <Text>🍏</Text>
+          </Right>
         </Header>
         <Content padder>
-            <Grid>
-                <Row style={{ paddingBottom: 10, justifyContent: 'center' }}>
-                    <Text style={{ textAlign: 'center'}}>タスクを追加してください</Text>
-                </Row>
-                <Row style={{ paddingBottom: 10 }}>
-                    <Col>
-                        <Item inlineLabel>
-                            <Label>Task</Label>
-                            <Input value={this.state.title} onChange={this.handleChange} placeholder="タスク名を入力" />
-                            <Button onPress={ this.addTask }><Text>Add</Text></Button>
-                        </Item>
-                    </Col>
-                </Row>
-            </Grid>
-            {
-                this.state.tasks.map( (data, idx) => {
-                    return (
-                        <Card key={idx}>
-                            <CardItem bordered>
-                                <Text>{data}</Text>
-                            </CardItem>
-                            <CardItem>
-                                <Body>
-                                    <Apples />
-                                </Body>
-                            </CardItem>
-                        </Card>
-                    )
-                })
-            }
+          <Grid>
+            <Row style={{ paddingBottom: 10, justifyContent: 'center' }}>
+              <Text style={{ textAlign: 'center' }}>
+                タスクを追加してください
+              </Text>
+            </Row>
+            <Row style={{ paddingBottom: 10 }}>
+              <Col>
+                <Item inlineLabel>
+                  <Label>Task</Label>
+                  <Input
+                    value={this.state.title}
+                    onChange={this.handleChange}
+                    placeholder="タスク名を入力"
+                  />
+                  <Button onPress={this.addTask}>
+                    <Text>Add</Text>
+                  </Button>
+                </Item>
+              </Col>
+            </Row>
+          </Grid>
+          {this.state.tasks.map((data, idx) => {
+            return (
+              <Card key={idx}>
+                <CardItem bordered>
+                  <Text>{data}</Text>
+                </CardItem>
+                <CardItem>
+                  <Body>
+                    <Apples />
+                  </Body>
+                </CardItem>
+              </Card>
+            )
+          })}
         </Content>
         <Footer>
-            <FooterTab>
-                <Button>
-                    <Text>Home</Text>
-                </Button>
-                <Button>
-                    <Text>Projects</Text>
-                </Button>
-                <Button>
-                    <Text>Lists</Text>
-                </Button>
-                <Button>
-                    <Text>Settings</Text>
-                </Button>
-            </FooterTab>
+          <FooterTab>
+            <Button>
+              <Text>Home</Text>
+            </Button>
+            <Button>
+              <Text>Projects</Text>
+            </Button>
+            <Button>
+              <Text>Lists</Text>
+            </Button>
+            <Button>
+              <Text>Settings</Text>
+            </Button>
+          </FooterTab>
         </Footer>
       </Container>
-    );
+    )
   }
 }
--- pomodoro/components/Apples.tsx
+++ pomodoro/components/Apples.tsx
@@ -1,52 +1,63 @@
-import React from 'react';
-import { AppLoading } from 'expo';
-import {
-    Container,
-    Text,
-    Button,
-    Grid,
-    Col
-} from 'native-base';
+import React from 'react'
+import { AppLoading } from 'expo'
+import { Container, Text, Button, Grid, Col } from 'native-base'
 
-export default class extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            value: 1.5,
-        }
-        this.increaseApple = this.increaseApple.bind(this)
-        this.decreaseApple = this.decreaseApple.bind(this)
+interface Props {}
+interface State {
+  value: number
+}
+
+export default class extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      value: 1.5,
     }
-    getApple() {
-        const apple = '🍎'
-        const apples = Array((this.state.value / 0.5)+ 1).join(apple)
-        return apples
-    }
-    increaseApple() {
-        const nowValue = this.state.value + 0.5
-        this.setState({ value: nowValue })
-    }
-    decreaseApple() {
-        const nowValue = this.state.value > 0 ? this.state.value - 0.5 : 0
-        this.setState({ value: nowValue })
-    }
-    render() {
-        return (
-            <Grid>
-                <Col style={{ width: 40, marginRight: 5 }}>
-                    <Button small bordered style={{alignSelf: 'flex-start'}} onPress={this.decreaseApple}>
-                        <Text>-</Text>
-                    </Button>
-                </Col>
-                <Col style={{ width: 40, marginRight: 10 }}>
-                    <Button small bordered style={{alignSelf: 'flex-start'}} onPress={this.increaseApple}>
-                        <Text>+</Text>
-                    </Button>
-                </Col>
-                <Col>
-                    <Text>{ this.getApple() } { this.state.value }h</Text>
-                </Col>
-            </Grid>
-        );
-    }
+    this.increaseApple = this.increaseApple.bind(this)
+    this.decreaseApple = this.decreaseApple.bind(this)
+  }
+  getApple() {
+    const apple = '🍎'
+    const apples = Array(this.state.value / 0.5 + 1).join(apple)
+    return apples
+  }
+  increaseApple() {
+    const nowValue = this.state.value + 0.5
+    this.setState({ value: nowValue })
+  }
+  decreaseApple() {
+    const nowValue = this.state.value > 0 ? this.state.value - 0.5 : 0
+    this.setState({ value: nowValue })
+  }
+  render() {
+    return (
+      <Grid>
+        <Col style={{ width: 40, marginRight: 5 }}>
+          <Button
+            small
+            bordered
+            style={{ alignSelf: 'flex-start' }}
+            onPress={this.decreaseApple}
+          >
+            <Text>-</Text>
+          </Button>
+        </Col>
+        <Col style={{ width: 40, marginRight: 10 }}>
+          <Button
+            small
+            bordered
+            style={{ alignSelf: 'flex-start' }}
+            onPress={this.increaseApple}
+          >
+            <Text>+</Text>
+          </Button>
+        </Col>
+        <Col>
+          <Text>
+            {this.getApple()} {this.state.value}h
+          </Text>
+        </Col>
+      </Grid>
+    )
+  }
 }
